e60f06a92cd9e39db5da2d77822008dbcaece540
Shujov <65087064+Shujov@users.noreply.github.com>
2022-11-07 14:55:13 -0800

    Solve fith exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const isNotNumeric = function(number){
+
+    return ((typeof number != "number") || isNaN(number))
+
+}
+
+const sumAll = function(firstNum, secondNum) {
+
+    if (isNotNumeric(firstNum) || isNotNumeric(secondNum)) return "ERROR";
+    else if(firstNum < 0 || secondNum < 0) return "ERROR";
+    else {
+
+        let result = 0;
+        let start = Math.min(firstNum, secondNum);
+        let end = Math.max(firstNum, secondNum);
+
+        for (let i=start; i<=end; i++){result+=i}
+
+        return result;
+
+    }
 
 };
 
