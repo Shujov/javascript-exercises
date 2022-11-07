70804e58eea053f65230544a2742f7e2c5db8866
Shujov <65087064+Shujov@users.noreply.github.com>
2022-11-07 15:07:21 -0800

    Solve sith exercise 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,21 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    // console.log("");
+    // console.log("Year: " + year);
+    // console.log("Divisible by 4: " + (year%4 === 0));
+    // console.log("Divisible by 100: " + (year%100 === 0));
+    // console.log("Divisible by 400: " + (year%400 === 0));
+
+    return ((year%4 === 0) && (year%100 != 0)) || ((year%400 === 0))
 
 };
+
+// console.log(leapYears(1996)) // true
+// console.log(leapYears(1997)) // false
+// console.log(leapYears(34992)) // true
+// console.log(leapYears(1900)) // false
+// console.log(leapYears(1600)) // true
+// console.log(leapYears(700)) // false
 
 // Do not edit below this line
 module.exports = leapYears;
