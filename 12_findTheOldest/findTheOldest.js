59a15e092d24cfa97b10b0e02d9b30fd04905cb7
Shujov <65087064+Shujov@users.noreply.github.com>
2022-11-09 15:15:58 -0800

    Solve exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    let oldest = {
+        name: "",
+        yearOfBirth: 2022,
+        yearOfDeath: 2022,
+    }
+
+    people.forEach(person => {
+
+        let oldestAge = (oldest.yearOfDeath || 2022) - (oldest.yearOfBirth);
+        let personAge = (person.yearOfDeath || 2022) - (person.yearOfBirth);
+
+        if (personAge > oldestAge) {
+            
+            oldest = person;
+
+        }
+        
+    });
+
+    return oldest;
 
 };
 
