45299377730a5c008c26c02c96c18aa8355a3c47
Shujov <65087064+Shujov@users.noreply.github.com>
2022-11-09 12:53:20 -0800

    Solve sith exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,41 @@
-const palindromes = function () {
+
+String.prototype.reverseString = function() { 
+
+    let result = "";
+
+    for (let i = (this.length - 1); i >= 0; i--) {result += this.charAt(i);}
+
+    return result;
 
 };
+
+String.prototype.removePunctuation = function() { 
+
+    return this.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+
+}
+
+String.prototype.removeBreaks = function() { 
+
+    return this.replace(/[\s]/g,"");
+
+}
+
+const palindromes = function (phrase) {
+
+    phrase = phrase.removeBreaks().removePunctuation().toLowerCase();
+
+    return (phrase === phrase.reverseString());
+
+};
+
+
+// console.log(palindromes('racecar')); /*(true);*/
+// console.log(palindromes('racecar!')); /*(true);*/
+// console.log(palindromes('Racecar!')); /*(true);*/
+// console.log(palindromes('A car, a man, a maraca.')); /*(true);*/
+// console.log(palindromes('Animal loots foliated detail of stool lamina.')); /*(true);*/
+// console.log(palindromes('ZZZZ car, a man, a maracaz.')); /*(false);*/
 
 // Do not edit below this line
 module.exports = palindromes;
