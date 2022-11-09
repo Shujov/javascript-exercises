248daa16ecb1a5e6837b8af2b0e3e203cc7d8d57
Shujov <65087064+Shujov@users.noreply.github.com>
2022-11-09 13:21:08 -0800

    Solve sith exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+
+    let fibonacciSeries = [1, 1];
+
+    if (index < 0){
+
+        return "OOPS";
+
+    } else if (index > 1) {
+
+        for (let i = 2; i < index; i++) {
+
+            fibonacciSeries.push(fibonacciSeries[i-1] + fibonacciSeries[i-2]);
+            
+        }
+
+    }
+
+    return fibonacciSeries[fibonacciSeries.length - 1];
 
 };
+
+// console.log(fibonacci(1));
+// console.log(fibonacci(0));
+// console.log(fibonacci(4));
+// console.log(fibonacci(5));
+// console.log(fibonacci(6));
 
 // Do not edit below this line
 module.exports = fibonacci;
